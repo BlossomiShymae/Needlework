0cc416d8580b35e602905971652aee202e2ea17e
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-06-20 20:00:48 -0500

    Setup tauri handler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,29 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   srcDir: "src",
+  app: {
+    head: {
+      meta: [
+        { name: "viewport", content: "width=device-width, intial-scale=1" },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css",
+          integrity:
+            "sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "/css/application.css",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.png",
+        },
+      ],
+    },
+  },
 });
---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello world!</h1>
+  <p>{{ word }}</p>
+</template>
+
+<script lang="ts" setup>
+import { invoke } from "@tauri-apps/api";
+
+const word = await invoke("greet", { name: "World" });
+</script>

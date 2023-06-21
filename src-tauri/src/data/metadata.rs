ecd2b57ba2ffae7c3edaf650ea09fdd0e12f9541
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-06-21 00:17:28 -0500

    Complete metadata objects

--- src-tauri/src/data/metadata.rs
+++ src-tauri/src/data/metadata.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::Deserialize;
 use serde::Serialize;
 use serde_json::Map;
@@ -9,7 +11,7 @@ pub struct LCUSchema {
     pub components: Components,
     pub info: Info,
     pub openapi: String,
-    pub paths: Map<String, Value>,
+    pub paths: HashMap<String, HashMap<String, Operation>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -22,5 +24,36 @@ pub struct Info {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Components {
-    pub schemas: Map<String, Value>,
+    pub schemas: HashMap<String, Schema>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Schema {
+    pub description: Option<String>,
+    pub properties: Option<Map<String, Value>>,
+    #[serde(rename = "type")]
+    pub _type: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Operation {
+    pub description: Option<String>,
+    pub operation_id: String,
+    pub parameters: Vec<Parameter>,
+    pub response: Option<Map<String, Value>>,
+    pub summary: Option<String>,
+    pub tags: Vec<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Parameter {
+    #[serde(rename = "in")]
+    pub _in: String,
+    pub name: String,
+    pub required: bool,
+    #[serde(rename = "type")]
+    pub _type: String,
 }

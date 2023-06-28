c16ebd330555e172123f4d0aaea81ed723e82210
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-06-28 16:40:48 -0500

    🦀 Make events a little prettier

--- src-tauri/src/server/events.rs
+++ src-tauri/src/server/events.rs
@@ -1,42 +1,69 @@
 use std::{
     error::Error,
-    time::{SystemTime, UNIX_EPOCH},
+    time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 use futures_util::StreamExt;
-use irelia::ws::{EventType, LCUWebSocket};
+use irelia::{
+    ws::{EventType, LCUWebSocket},
+    LCUError,
+};
 use tauri::{App, Manager};
+use tokio::time::{sleep_until, Instant};
 
 use crate::data::models::{WebSocketMessage, WebSocketPayload};
 
 pub fn inject_events(app: &mut App) -> Result<(), Box<dyn Error>> {
     let handle = app.handle();
     tauri::async_runtime::spawn(async move {
-        let mut ws = LCUWebSocket::new().await.unwrap();
-        ws.subscribe(EventType::OnJsonApiEvent);
+        // Loop that handles the lifetime of the LCU
         loop {
-            let _handle = handle.clone();
-            if let Some(event) = ws.next().await {
-                let _ = event.map(|v| {
-                    let message = v.as_array().unwrap();
-                    let opcode = message.get(0).unwrap().as_i64().unwrap();
-                    let event = message.get(1).unwrap().as_str().unwrap();
-                    let payload: WebSocketPayload =
-                        serde_json::from_value(message.get(2).unwrap().clone()).unwrap();
-                    let wsm = WebSocketMessage {
-                        opcode,
-                        event: event.into(),
-                        data: payload.data,
-                        event_type: payload.event_type,
-                        uri: payload.uri,
-                        timestamp: SystemTime::now()
-                            .duration_since(UNIX_EPOCH)
-                            .unwrap()
-                            .as_millis(),
-                    };
-                    let _ = _handle.emit_all("lcu-ws-event", wsm);
-                });
-            };
+            match LCUWebSocket::new().await {
+                Ok(mut ws) => {
+                    ws.subscribe(EventType::OnJsonApiEvent);
+                    // Loop that handles incoming WebSocket events
+                    let _handle = handle.clone();
+                    while let Some(event) = ws.next().await {
+                        match event {
+                            Ok(v) => {
+                                let message = v.as_array().unwrap();
+                                let opcode = message.get(0).unwrap().as_i64().unwrap();
+                                let event = message.get(1).unwrap().as_str().unwrap();
+                                let payload: WebSocketPayload =
+                                    serde_json::from_value(message.get(2).unwrap().clone())
+                                        .unwrap();
+                                let wsm = WebSocketMessage {
+                                    opcode,
+                                    event: event.into(),
+                                    data: payload.data,
+                                    event_type: payload.event_type,
+                                    uri: payload.uri,
+                                    timestamp: SystemTime::now()
+                                        .duration_since(UNIX_EPOCH)
+                                        .unwrap()
+                                        .as_millis(),
+                                };
+                                let _ = _handle.emit_all("lcu-ws-event", wsm);
+                            }
+                            Err(e) => {
+                                match e {
+                                    LCUError::LCUProcessNotRunning => {
+                                        // Break out of incoming event loop
+                                        ws.terminate();
+                                        break;
+                                    }
+                                    _ => println!("{:?}", e),
+                                }
+                            }
+                        };
+                    }
+                }
+                // The League client is probs not running
+                Err(_) => {
+                    // Wait a second before trying again
+                    sleep_until(Instant::now() + Duration::from_secs(1)).await;
+                }
+            }
         }
     });
 

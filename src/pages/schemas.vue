9b0e85b4a7f7629fa10dd2376bf85ea2cf788aa8
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-06-28 06:18:30 -0500

    🔎 Add filter for schemas

--- src/pages/schemas.vue
+++ src/pages/schemas.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="p-2">
     <h5>Schemas</h5>
-    <div v-for="schema in schemas" class="border mb-2">
+    <div class="input-group input-group-sm mb-2 align-items-center">
+      <span class="input-group-text">Filter</span>
+      <input type="text" class="form-control" v-model="filter" />
+    </div>
+    <div v-for="schema in filteredSchemas" class="border mb-2">
       <Schema
         :name="schema.name"
         :description="schema.description"
@@ -15,12 +19,23 @@
 </template>
 
 <script lang="ts" setup>
-import { inject } from "vue";
+import { inject, ref, computed } from "vue";
 import { Invoker } from "~/composables/invoker";
 
 const invoker = inject(Invoker.Key) as Invoker;
 
-const schemas = await invoker.schemas();
+const schemas: any[] = [];
+const _schemas = (await invoker.schemas()) as any;
+for (const [k, v] of Object.entries(_schemas)) {
+  schemas.push(v);
+}
+const filter = ref("");
+const filteredSchemas = computed(() => {
+  return schemas.filter((x) => {
+    if (filter.value === "") return true;
+    return x.name.toLowerCase().indexOf(filter.value.toLowerCase()) > -1;
+  });
+});
 
 definePageMeta({
   layout: "window",
